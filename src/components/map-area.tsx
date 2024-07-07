7467e56a4ce172d8b95a2b77a231b69f31c2e32c
Matheus Freitas <matheusmatheusfdc10@hotmail.com>
2024-07-06 22:08:48 -0300

    up map end search

--- src/components/map-area.tsx
+++ src/components/map-area.tsx
@@ -1,53 +1,64 @@
+"use client"
 import { GoogleMap, LoadScript, Circle, Marker } from '@react-google-maps/api';
 
 interface MapAreaProps {
-  lat: number;
-  lng: number;
-  distance: number; // distance in kilometers
-  name: string;
+  lat?: number;
+  lng?: number;
+  distance?: number; // distance in kilometers
+  name?: string;
+  zoom?: number;
+  children?: React.ReactNode;
 }
 
-const MapArea: React.FC<MapAreaProps> = ({ lat, lng, distance, name }) => {
-  const mapContainerStyle = {
-    height: '400px',
-    width: '100%',
-  };
-
+const MapArea: React.FC<MapAreaProps> = ({ lat, lng, distance, name = 'Você', zoom = 13, children }) => {  
   const center = {
-    lat: lat,
-    lng: lng,
+    lat: lat || -22.9035,
+    lng: lng || -43.2096,
   };
 
-  const circleOptions = {
-    strokeColor: '#FF0000',
-    strokeOpacity: 0.8,
-    strokeWeight: 2,
-    fillColor: '#FF0000',
-    fillOpacity: 0.35,
-    clickable: false,
-    draggable: false,
-    editable: false,
-    visible: true,
-    radius: distance * 1000, // Convert km to meters
-    zIndex: 1,
-  };
+  const loading = (
+    <div className='h-[400px] w-full flex justify-center items-center'>
+      <div>Loading...</div>
+    </div>
+  )
 
   return (
-    <LoadScript googleMapsApiKey={''}>
+    <LoadScript googleMapsApiKey={''} loadingElement={loading}>
       <GoogleMap
-        mapContainerStyle={mapContainerStyle}
+        mapContainerStyle={{
+          height: '400px',
+          width: '100%',
+          borderRadius: 8
+        }}
         center={center}
-        zoom={13} // Adjust zoom level as needed
+        zoom={zoom} // Adjust zoom level as needed
       >
-        <Marker position={center} options={{
+        {children}
+        {!!(lat || lng) && (
+          <Marker position={center} options={{
             label: {
-                text: name,
-                className: '-mt-8',
-                fontWeight: '700',
-                color: 'white'
+              text: name,
+              className: '-mt-8',
+              fontWeight: '700',
+              color: 'white'
             }
-        }}/>
-        <Circle center={center} options={circleOptions} />
+          }}/>
+        )}
+        {distance && (
+          <Circle center={center} options={{
+            strokeColor: '#FF0000',
+            strokeOpacity: 0.8,
+            strokeWeight: 2,
+            fillColor: '#FF0000',
+            fillOpacity: 0.35,
+            clickable: false,
+            draggable: false,
+            editable: false,
+            visible: true,
+            radius: distance * 1000, // Convert km to meters
+            zIndex: 1,
+          }} />
+        )}
       </GoogleMap>
     </LoadScript>
   );
--- src/components/search-address.tsx
+++ src/components/search-address.tsx
@@ -6,6 +6,8 @@ import { useDebounceValue } from "usehooks-ts";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { SearchAddressType, searchAddress } from "@/actions/address";
+import MapArea from "@/components/map-area";
+import { Button } from "./ui/button";
 
 interface SearchAddressProsp {
     onChange: (address: SearchAddressType) => void;
@@ -21,8 +23,9 @@ const SearchAddress: React.FC<SearchAddressProsp> = ({
     disabled,
 }) => {
     const [adresses, setAdresses] = useState<SearchAddressType[]>([]);
-    const [address, setAddress] = useState<string>('');
-    const debouncedValue = useDebounceValue(address, 500);
+    const [address, setAddress] = useState<SearchAddressType | null>(null);
+    const [textAddress, setTextAddress] = useState<string>('');
+    const debouncedValue = useDebounceValue(textAddress, 500);
 
     const fetchAddresses = useCallback(async () => {
         if (debouncedValue[0].length >= 3) {
@@ -34,7 +37,7 @@ const SearchAddress: React.FC<SearchAddressProsp> = ({
         }
     }, [debouncedValue[0]]);
 
-    const handleOnChange = (item: SearchAddressType) => {
+    const handleSelectAddress = (item: SearchAddressType) => {
         if (
             item.city &&
             item.country &&
@@ -47,8 +50,8 @@ const SearchAddress: React.FC<SearchAddressProsp> = ({
             item.uf &&
             item.zipCode
         ) {
-            onChange(item);
-            setAddress("")
+            setAddress(item)
+            // setTextAddress("")
             setAdresses([])
         }
     }
@@ -58,27 +61,58 @@ const SearchAddress: React.FC<SearchAddressProsp> = ({
     }, [debouncedValue[0]]);
 
     return (
-        <div className="relative">
-            <div className="space-y-2">
-                {textLabel && (
-                    <Label>
-                        {textLabel}
-                    </Label>
+        <div className="space-y-6">
+            <div className="relative">
+                <div className="space-y-2">
+                    {textLabel && (
+                        <Label>
+                            {textLabel}
+                        </Label>
+                    )}
+                    <Input
+                        value={textAddress}
+                        onChange={(e) => {
+                            setTextAddress(e.target.value)
+                            setAddress(null)
+                        }}
+                        placeholder={placeholder}
+                        disabled={disabled}
+                    />
+                </div>
+                {!!adresses.length && (
+                    <ul className="divide-y divide-gray-300 absolute top-full mt-1 w-full bg-white z-10 max-h-60 overflow-y-auto border-b border-x">
+                        {adresses.map((address, key) => (
+                            <li 
+                                key={key}
+                                onClick={() => handleSelectAddress(address)}
+                                className="flex py-2 cursor-pointer hover:bg-zinc-50 active:scale-[.99]"
+                            >
+                                <div className="flex justify-center items-center w-10">
+                                    <PiMapPinLight className="h-5 w-5 text-muted-foreground" />
+                                </div>
+                                <div className="flex flex-col flex-1 mr-2">
+                                    <span>
+                                        {[address.streetAddress, address.number].filter(Boolean).join(', ')}
+                                    </span>
+                                    <span className="text-sm text-muted-foreground">
+                                        {[address.neighborhood, address.city, address.uf, address.country].filter(Boolean).join(' - ')}
+                                    </span>
+                                </div>
+                            </li>
+                        ))}
+                    </ul>
                 )}
-                <Input
-                    value={address}
-                    onChange={(e) => setAddress(e.target.value)}
-                    placeholder={placeholder}
-                    disabled={disabled}
-                />
             </div>
-            {!!adresses.length && (
-                <ul className="divide-y divide-gray-300 absolute top-full mt-1 w-full bg-white z-10 max-h-60 overflow-y-auto border-b border-x">
-                    {adresses.map((address, key) => (
-                        <li 
-                            key={key}
-                            onClick={() => handleOnChange(address)}
-                            className="flex py-2 cursor-pointer hover:bg-zinc-50 active:scale-[.99]"
+            
+            <MapArea
+                lat={Number(address?.latitude)}
+                lng={Number(address?.longitude)}
+                zoom={address ? 16 : 11}
+            >
+                {address && (
+                    <div className="absolute top-[10px] left-[10px] space-y-4">
+                        <div
+                            className="bg-white rounded-sm flex py-2 cursor-pointer hover:bg-zinc-50 active:scale-[.99]"
                         >
                             <div className="flex justify-center items-center w-10">
                                 <PiMapPinLight className="h-5 w-5 text-muted-foreground" />
@@ -91,10 +125,16 @@ const SearchAddress: React.FC<SearchAddressProsp> = ({
                                     {[address.neighborhood, address.city, address.uf, address.country].filter(Boolean).join(' - ')}
                                 </span>
                             </div>
-                        </li>
-                    ))}
-                </ul>
-            )}
+                        </div>
+                        <Button
+                            onClick={() => onChange(address)}
+                            variant="outline"
+                        >
+                            Confirmar
+                        </Button>
+                    </div>
+                )}
+            </MapArea>
         </div>
     );
 }
